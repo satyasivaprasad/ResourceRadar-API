a84ddfd4491dfd532d27b24a5e49484eb493a984
Manjufission <manju.chigilipalli@fissionlabs.com>
2023-05-17 13:11:09 +0530

    Login implementation

---
+++ src/main/java/com/resourceradar/security/SecurityLoginFilter.java
@@ -0,0 +1,61 @@
+package com.resourceradar.security;
+
+import java.io.IOException;
+
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.resourceradar.dto.LoginRequestDTO;
+import com.resourceradar.enums.ProviderEnum;
+import com.resourceradar.exception.ResourceNotFoundException;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class SecurityLoginFilter extends AbstractAuthenticationProcessingFilter {
+
+    private TokenService tokenService;
+
+    protected SecurityLoginFilter(RequestMatcher requiresAuthenticationRequestMatcher,
+                                  AuthenticationManager authenticationManager, TokenService tokenService) {
+        super(requiresAuthenticationRequestMatcher, authenticationManager);
+        this.tokenService = tokenService;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+            throws AuthenticationException, IOException, ServletException {
+        final LoginRequestDTO loginDTO = new ObjectMapper().readValue(request.getInputStream(), LoginRequestDTO.class);
+
+        AbstractAuthenticationToken token = null;
+        if (loginDTO.getProvider().equals(ProviderEnum.Google.toString())) {
+            token = new GoogleAuthToken(null, loginDTO.getCredentials(), null);
+        }
+        return getAuthenticationManager().authenticate(token);
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+                                            Authentication authentication) throws IOException, ServletException {
+        GoogleAuthToken tokenObj = (GoogleAuthToken) authentication;
+        log.info(tokenObj.toString());
+
+        try {
+            tokenService.addTokenToAuthHeader((String) tokenObj.getPrincipal(), response);
+        } catch (ResourceNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
+
+}
